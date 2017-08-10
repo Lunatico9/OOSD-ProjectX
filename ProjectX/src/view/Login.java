6b6224e43b8b00167b83a0ce7cbb9dbbc725a71f
Lorenzo Collevecchio <Lorenzo Collevecchio@MSI.fritz.box>
2017-08-10 15:21:07 +0200

    Login page

--- ProjectX/src/view/Login.java
+++ ProjectX/src/view/Login.java
@@ -1,5 +1,89 @@
-package view;
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class Login {
 
+	private JFrame frmLogin;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					loginPage window = new loginPage();
+					window.frmLogin.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public loginPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmLogin = new JFrame();
+		frmLogin.setTitle("Login");
+		frmLogin.setBounds(100, 100, 700, 500);
+		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLogin.getContentPane().setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Login");
+		lblLogin.setForeground(Color.BLACK);
+		lblLogin.setFont(new Font("Arial", Font.PLAIN, 40));
+		lblLogin.setBounds(89, 93, 100, 48);
+		frmLogin.getContentPane().add(lblLogin);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setForeground(Color.BLACK);
+		lblUsername.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblUsername.setBounds(89, 189, 100, 29);
+		frmLogin.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setForeground(Color.BLACK);
+		lblPassword.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblPassword.setBounds(89, 243, 100, 29);
+		frmLogin.getContentPane().add(lblPassword);
+		
+		textField = new JTextField();
+		textField.setBounds(220, 192, 322, 27);
+		frmLogin.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(220, 246, 322, 27);
+		frmLogin.getContentPane().add(passwordField);
+		
+		JButton btnAccedi = new JButton("Accedi");
+		btnAccedi.setFont(new Font("Arial", Font.PLAIN, 20));
+		btnAccedi.setBounds(220, 323, 132, 48);
+		frmLogin.getContentPane().add(btnAccedi);
+		
+		JButton btnRegistrati = new JButton("Registrati");
+		btnRegistrati.setFont(new Font("Arial", Font.PLAIN, 20));
+		btnRegistrati.setBounds(410, 323, 132, 48);
+		frmLogin.getContentPane().add(btnRegistrati);
+	}
 }
