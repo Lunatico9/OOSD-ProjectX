a44aabc6b11116be9f75c4585fc70e4e0a9248bc
Lorenzo Collevecchio <Lorenzo Collevecchio@MSI.fritz.box>
2017-08-10 16:39:14 +0200

    AddReview update

--- ProjectX/src/view/AddReview.java
+++ ProjectX/src/view/AddReview.java
@@ -1,5 +1,62 @@
 package view;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.Font;
+
 public class AddReview {
 
+	private JFrame frmAddReview;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddReview window = new AddReview();
+					window.frmAddReview.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AddReview() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmAddReview = new JFrame();
+		frmAddReview.setTitle("Add review");
+		frmAddReview.setBounds(100, 100, 700, 500);
+		frmAddReview.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAddReview.getContentPane().setLayout(null);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(86, 101, 500, 200);
+		frmAddReview.getContentPane().add(textPane);
+		
+		JButton btnNewButton = new JButton("Aggiungi");
+		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 20));
+		btnNewButton.setBounds(86, 346, 132, 48);
+		frmAddReview.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Annulla");
+		btnNewButton_1.setFont(new Font("Arial", Font.PLAIN, 20));
+		btnNewButton_1.setBounds(454, 346, 132, 48);
+		frmAddReview.getContentPane().add(btnNewButton_1);
+	}
 }
+
