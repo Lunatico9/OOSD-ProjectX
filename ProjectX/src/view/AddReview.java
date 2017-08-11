834a34bfe8e05fe5e60c1e8f860a04c2027b8be5
Lorenzo Collevecchio <Lorenzo Collevecchio@MSI.fritz.box>
2017-08-11 12:19:03 +0200

    AddReview update jpanel

--- ProjectX/src/view/AddReview.java
+++ ProjectX/src/view/AddReview.java
@@ -4,11 +4,13 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTextPane;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import java.awt.Color;
+import javax.swing.JPanel;
 
 public class AddReview {
 
@@ -48,34 +50,37 @@ public class AddReview {
 		frmAddReview.getContentPane().setLayout(null);
 		
 		JTextPane textPane = new JTextPane();
-		textPane.setBounds(86, 73, 500, 200);
+		textPane.setBounds(110, 72, 450, 200);
 		frmAddReview.getContentPane().add(textPane);
 		
 		JButton btnNewButton = new JButton("Aggiungi");
 		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 20));
-		btnNewButton.setBounds(86, 374, 132, 48);
+		btnNewButton.setBounds(298, 352, 132, 48);
 		frmAddReview.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Annulla");
 		btnNewButton_1.setFont(new Font("Arial", Font.PLAIN, 20));
-		btnNewButton_1.setBounds(454, 374, 132, 48);
+		btnNewButton_1.setBounds(470, 352, 132, 48);
 		frmAddReview.getContentPane().add(btnNewButton_1);
-		
-		JLabel lblReview = new JLabel("Review:");
-		lblReview.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblReview.setBounds(86, 32, 86, 40);
-		frmAddReview.getContentPane().add(lblReview);
-		
-		JLabel lblVote = new JLabel("Vote:");
-		lblVote.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblVote.setBounds(86, 286, 86, 40);
-		frmAddReview.getContentPane().add(lblVote);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBackground(Color.WHITE);
 		comboBox.setFont(new Font("Arial", Font.PLAIN, 20));
-		comboBox.setBounds(86, 327, 70, 28);
+		comboBox.setBounds(110, 362, 70, 28);
 		frmAddReview.getContentPane().add(comboBox);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(65, 38, 537, 269);
+		frmAddReview.getContentPane().add(panel);
+		panel.setBorder(BorderFactory.createTitledBorder(
+		BorderFactory.createEtchedBorder(), "RECENSIONE"));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(65, 339, 158, 68);
+		frmAddReview.getContentPane().add(panel_1);
+		panel_1.setBorder(BorderFactory.createTitledBorder(
+		BorderFactory.createEtchedBorder(), "VOTO"));
+		
 		for(int i = 1; i < 11 ; i++)
 		comboBox.addItem(i);
 	}
