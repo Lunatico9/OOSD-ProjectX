fdbf012949db7a521e9780031d116628dba35b14
Win10 <Win10@192.168.1.110>
2017-08-16 11:53:22 +0200

    registration update

--- ProjectX/src/controller/AddGameController.java
+++ ProjectX/src/controller/AddGameController.java
@@ -9,7 +9,7 @@ public class AddGameController {
 	public static void AddGame(String title, String genere, String data) throws Exception {
 		Game a = new Game(title, data, genere);
 		System.out.println("Gioco " + title + " uscito nel " + data + " aggiunto" );
-		String sqlQuery = "INSERT INTO `game` (`idGame`, `name`, `points`) VALUES ('13', '" + title + "' , '10')";
+		String sqlQuery = "INSERT INTO `game` (`idGame`, `name`) VALUES ('13', '" + title + "')";
 		ResultSet rst = DatabaseMySQL.SendQuery(sqlQuery);	
 	}
 }
