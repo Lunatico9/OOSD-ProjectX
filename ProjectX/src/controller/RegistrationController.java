2aa0adec159aa079ae521d8bf6e86ffa162d9eca
Win10 <Win10@192.168.1.109>
2017-09-04 11:29:32 +0200

    Ricommitto registration e registration controller per piccole modifiche

--- ProjectX/src/controller/RegistrationController.java
+++ ProjectX/src/controller/RegistrationController.java
@@ -2,14 +2,17 @@ package controller;
 
 import java.sql.ResultSet;
 
+import javax.swing.JOptionPane;
+
 import database.DatabaseMySQL;
 import model.Actor;
 
 public class RegistrationController {
-	
-	public static boolean verify(String username){
-		String sqlQuery = "SELECT username FROM user";
-		if(username.equals(sqlQuery)){ 
+
+	public static boolean verify(String username) throws Exception{
+		String sqlQuery = "SELECT username FROM user WHERE username='"+ username + "'";
+		ResultSet rst= DatabaseMySQL.SendQuery(sqlQuery);
+		if(rst.next()){
 			 return true; 
 				}
 		else return false;
@@ -23,8 +26,9 @@ public class RegistrationController {
 	public static void AddUser(String Username, String Password, String Name, String Surname, String Email) throws Exception{
 		Actor x = new Actor(Username,Password,Name,Surname);
 		String Tipo= "User";
-		int IDuser= 15;
-		String sqlQuery = "INSERT INTO `user` (`username`, `password`, `type`,`name`,`surname`,`email`,`idUser` ) VALUES ('" + Username + "', '" + Password + "', '"+ Tipo +"', '"+ Name +"', '"+ Surname +"', '"+ Email +"', '"+ IDuser +"')"; 
+		int IDuser=17;
+		String sqlQuery = "INSERT INTO `user` (`username`, `password`, `type`,`name`,`surname`,`email`,`idUser` ) VALUES ('" + Username + "', '" + Password + "', '"+ Tipo +"', '"+ Name +"', '"+ Surname +"', '"+ Email +"', '"+ IDuser +"')";
 		ResultSet rst = DatabaseMySQL.SendQuery(sqlQuery);
+		JOptionPane.showMessageDialog(null, "L'utente "+Username+" è stato registrato");
 	}
 }
