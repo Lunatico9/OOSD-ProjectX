a2448e6305c26e5eb4f722ad667e80cbb2472ac5
Lorenzo Collevecchio <Lorenzo Collevecchio@MSI.fritz.box>
2017-09-04 12:39:53 +0200

    Bozza

--- ProjectX/src/view/PromotionDemotionModerator.java
+++ ProjectX/src/view/PromotionDemotionModerator.java
@@ -3,10 +3,23 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import database.DatabaseMySQL;
+import model.Actor;
+
+import java.awt.Font;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.swing.border.LineBorder;
+import java.awt.Color;
 
 public class PromotionDemotionModerator {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -27,17 +40,46 @@ public class PromotionDemotionModerator {
 	/**
 	 * Create the application.
 	 */
-	public PromotionDemotionModerator() {
+	public PromotionDemotionModerator() throws Exception {
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() throws Exception {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 700, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		ArrayList<String> users = new ArrayList<String>();
+		String query = "SELECT username FROM user";
+		ResultSet rst = DatabaseMySQL.SendQuery(query);
+		while(rst.next()) {
+			users.add(rst.getString(1));
+		}
+		
+		table = new JTable();
+		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"Username", "Type"
+			}
+		));
+		table.setFont(new Font("Arial", Font.PLAIN, 20));
+		table.setBounds(174, 109, 200, 160);
+		frame.getContentPane().add(table);
 	}
-
 }
