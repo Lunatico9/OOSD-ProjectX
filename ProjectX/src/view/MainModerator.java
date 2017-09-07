1e53fd14bb631d4d628b61428319956871b55899
Matteo <Matteo@LAPTOP-8JHTB2B8.fritz.box>
2017-09-07 14:52:59 +0200

    MainMod

--- ProjectX/src/view/MainModerator.java
+++ ProjectX/src/view/MainModerator.java
@@ -1,5 +1,75 @@
 package view;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class MainModerator {
 
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainModerator window = new MainModerator();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainModerator() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Gestisci utenti");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.dispose();
+				PromotionDemotionModerator.main(null);
+			}
+		});
+		btnNewButton.setBounds(41, 27, 198, 36);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnGestisciNuoveRecensioni = new JButton("Gestisci nuove recensioni");
+		btnGestisciNuoveRecensioni.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				AcceptReview.main(null);
+			}
+		});
+		btnGestisciNuoveRecensioni.setBounds(41, 74, 198, 36);
+		frame.getContentPane().add(btnGestisciNuoveRecensioni);
+		
+		JButton btnUtilizzaAccountCome = new JButton("Utilizza account come utente");
+		btnUtilizzaAccountCome.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MainUser.main(null);
+			}
+		});
+		btnUtilizzaAccountCome.setBounds(41, 121, 198, 36);
+		frame.getContentPane().add(btnUtilizzaAccountCome);
+	}
 }
