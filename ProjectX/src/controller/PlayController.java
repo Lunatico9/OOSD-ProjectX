b03e85d039f6a490b8a7c1cf4fd2a994ee95b6f4
Win10 <Win10@Franz>
2017-09-11 12:58:07 +0200

    Play Controller

--- ProjectX/src/controller/PlayController.java
+++ ProjectX/src/controller/PlayController.java
@@ -18,47 +18,47 @@ public class PlayController {
 			if(exp==100||exp==300||exp==600||exp==1000||exp==1500||exp==2100||exp==2800||exp==3600||exp==4500){
 				if(exp==100){
 					qdu="UPDATE user SET level=2 WHERE username = '" + username + "'";
-					ResultSet lv= DatabaseMySQL.SendQuery(qdu);
+					ResultSet lvl= DatabaseMySQL.SendQuery(qdu);
 					JOptionPane.showMessageDialog(null, "Congratulazioni sei salito al Livello 2!");
 				}
 				if(exp==300){
 					qdu="UPDATE user SET level=3 WHERE username = '" + username + "'";
-					ResultSet lv= DatabaseMySQL.SendQuery(qdu);
+					ResultSet lvl= DatabaseMySQL.SendQuery(qdu);
 					JOptionPane.showMessageDialog(null, "Congratulazioni sei salito al Livello 3!");
 				}
 				if(exp==600){
 					qdu="UPDATE user SET level=4 WHERE username = '" + username + "'";
-					ResultSet lv= DatabaseMySQL.SendQuery(qdu);
+					ResultSet lvl= DatabaseMySQL.SendQuery(qdu);
 					JOptionPane.showMessageDialog(null, "Congratulazioni sei salito al Livello 4!");
 				}
 				if(exp==1000){
 					qdu="UPDATE user SET level=5 WHERE username = '" + username + "'";
-					ResultSet lv= DatabaseMySQL.SendQuery(qdu);
+					ResultSet lvl= DatabaseMySQL.SendQuery(qdu);
 					JOptionPane.showMessageDialog(null, "Congratulazioni sei salito al Livello 5!");
 				}
 				if(exp==1500){
 					qdu="UPDATE user SET level=6 WHERE username = '" + username + "'";
-					ResultSet lv= DatabaseMySQL.SendQuery(qdu);
+					ResultSet lvl= DatabaseMySQL.SendQuery(qdu);
 					JOptionPane.showMessageDialog(null, "Congratulazioni sei salito al Livello 6!");
 				}
 				if(exp==2100){
 					qdu="UPDATE user SET level=7 WHERE username = '" + username + "'";
-					ResultSet lv= DatabaseMySQL.SendQuery(qdu);
+					ResultSet lvl= DatabaseMySQL.SendQuery(qdu);
 					JOptionPane.showMessageDialog(null, "Congratulazioni sei salito al Livello 7!");
 				}
 				if(exp==2800){
 					qdu="UPDATE user SET level=8 WHERE username = '" + username + "'";
-					ResultSet lv= DatabaseMySQL.SendQuery(qdu);
+					ResultSet lvl= DatabaseMySQL.SendQuery(qdu);
 					JOptionPane.showMessageDialog(null, "Congratulazioni sei salito al Livello 8!");
 				}
 				if(exp==3600){
 					qdu="UPDATE user SET level=9 WHERE username = '" + username + "'";
-					ResultSet lv= DatabaseMySQL.SendQuery(qdu);
+					ResultSet lvl= DatabaseMySQL.SendQuery(qdu);
 					JOptionPane.showMessageDialog(null, "Congratulazioni sei salito al Livello 9!");
 				}
 				if(exp==4500){
 					qdu="UPDATE user SET level=10 WHERE username = '" + username + "'";
-					ResultSet lv= DatabaseMySQL.SendQuery(qdu);
+					ResultSet lvl= DatabaseMySQL.SendQuery(qdu);
 					JOptionPane.showMessageDialog(null, "Congratulazioni sei salito al Livello 10!");
 				}
 			}
