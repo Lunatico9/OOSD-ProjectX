dfcad5d52e4c0b62eb11371dae7c48904620f3c0
Win10 <Win10@Franz>
2017-09-12 03:35:21 +0200

    Quisquiglie

--- ProjectX/src/controller/AddGameController.java
+++ ProjectX/src/controller/AddGameController.java
@@ -9,7 +9,13 @@ public class AddGameController {
 	public static void AddGame(String title, String genere, String data) throws Exception {
 		Game a = new Game(title, data, genere);
 		System.out.println("Gioco " + title + " uscito nel " + data + " aggiunto" );
-		String sqlQuery = "INSERT INTO `game` (`idGame`, `name`) VALUES ('15', '" + title + "')";
+		int IDGAME=0;
+		String sql="SELECT MAX(idGame) FROM game";
+		String sqlQuery = "INSERT INTO `game` (`idGame`, `name`) VALUES ('"+IDGAME+"','" + title + "')";
+		ResultSet rs= DatabaseMySQL.SendQuery(sql);
+		if(rs.next()){
+			IDGAME=rs.getInt("idGame")+1;
+		}
 		ResultSet rst = DatabaseMySQL.SendQuery(sqlQuery);	
 	}
 }
